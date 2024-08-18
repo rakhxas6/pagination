f5be087b6af4f590ec3004e290283467f3c88564
Sudhan Kandel <sujan.kandel.0ne8@gmail.com>
2024-08-18 11:02:41 +0545

    made changes to ensure the pagination acts right

--- src/components/FrontendPagination.jsx
+++ src/components/FrontendPagination.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const FrontendPagination = () => {
   const [products, setProducts] = useState([]);
-
   const [page, setPage] = useState(1);
 
   const fetchProducts = async () => {
@@ -16,23 +14,28 @@ const FrontendPagination = () => {
   };
 
   const selectedPageHandler = (selectedPage) => {
+    const totalPages = Math.ceil(products.length / 10);
     if (
       selectedPage >= 1 &&
-      selectedPage <= products.length / 10 &&
+      selectedPage <= totalPages &&
       selectedPage !== page
-    )
+    ) {
       setPage(selectedPage);
-     window.scrollTo({ top: 0, behavior: "smooth" });
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
   };
 
   useEffect(() => {
     fetchProducts();
   }, []);
+
+  const totalPages = Math.ceil(products.length / 10);
+
   return (
     <>
       {products.length > 0 && (
         <div className="products">
-          {products.slice(page * 10 - 10, page * 10).map((product) => {
+          {products.slice((page - 1) * 10, page * 10).map((product) => {
             return (
               <span className="products__single" key={product.id}>
                 <img src={product.thumbnail} alt={product.title} />
@@ -45,27 +48,28 @@ const FrontendPagination = () => {
       {products.length > 0 && (
         <div className="pagination">
           <span
-            className={page > 1 ? " " : "pagination__disabled"}
+            className={page > 1 ? "" : "pagination__disabled"}
             onClick={() => selectedPageHandler(page - 1)}
+            aria-label="Previous Page"
           >
             ◀
           </span>
-          {[...Array(products.length / 10)].map((_, i) => {
+          {[...Array(totalPages)].map((_, i) => {
             return (
               <span
                 className={page === i + 1 ? "pagination__selected" : ""}
                 onClick={() => selectedPageHandler(i + 1)}
                 key={i}
+                aria-label={`Page ${i + 1}`}
               >
                 {i + 1}
               </span>
             );
           })}
           <span
-            className={
-              page < products.length / 10 ? " " : "pagination__disabled"
-            }
+            className={page < totalPages ? "" : "pagination__disabled"}
             onClick={() => selectedPageHandler(page + 1)}
+            aria-label="Next Page"
           >
             ▶
           </span>
